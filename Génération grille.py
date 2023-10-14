30d0d7f0b7978ffa9880940e11d01d5f6b32c184
UserMika <71926176+UserMika@users.noreply.github.com>
2023-10-14 21:39:22 +0200

    Génération tiles

---
+++ Génération grille.py
@@ -0,0 +1,42 @@
+import pygame
+pygame.init()
+screen = pygame.display.set_mode((800,800))
+done = False
+TILE_SIZE = 16
+
+def iso_coord(x, y):
+    return (x-y, (x+y)/2)
+
+def update_window(window):
+    window.fill((255,255,255))
+    pygame.display.update()
+
+
+def display_tiles(screen, value):
+    screen_x, screen_y = screen.get_size()
+    print(screen_x, screen_y)
+    for y in range(value):
+        for x in range(value):
+            new_tile = Tile(screen_x//2 + x*TILE_SIZE, y*TILE_SIZE)
+            screen.blit(new_tile.image, iso_coord(new_tile.rect.x, new_tile.rect.y))
+
+
+
+class Tile():
+    def __init__(self, x, y):
+        self.image = pygame.image.load("Real Tile.png")
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        
+
+
+update_window(screen)
+display_tiles(screen,20)
+pygame.display.update()
+
+
+while not done:
+    for event in  pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
