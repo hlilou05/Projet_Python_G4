2dc4a5ea2216a48ed449dd080d2d3a10b3789dc2
Titouan <108216125+Titou2003@users.noreply.github.com>
2023-11-13 00:22:04 +0100

    Add files via upload

--- V3.py
+++ V3.py
@@ -3,18 +3,19 @@
 # Titouan GODARD
 
 from random import *
+from time import *
 
 ################ VARIABLES ######################
 
-bobsQty = 50 #quantité initiale de Bobs.
-foodQty = 5 #quantité d'items Food sur la map générés au départ et par jour de jeu.
+bobsQty = 10 #quantité initiale de Bobs.
+foodQty = 10 #quantité d'items Food sur la map générés au départ et par jour de jeu.
 foodEnergy = 20 #quantité d'énergie d'un item de food généré sur la map.
 energyInitLevel = 100 #Niveau d'énergie initial des Bobs.
 gridSize = 10 #Taille de la grille carrée.
 maxEnergy = 200 #Energie max d'un Bob.
 birthEnergy = 50
 motherEnergy = 150
-ticksPerDay = 100 #Nombre de Ticks par jour
+ticksPerDay = 50 #Nombre de Ticks par jour
 
 
 
@@ -40,13 +41,23 @@ class Bob:
     #parametre recu pour chaque fonction : La case dans laquelle se trouve le bob.
 
     def partheno(self, key):
-        if self.energy>maxEnergy:
+        if self.energy==maxEnergy:
             self.energy -= motherEnergy
             if key not in gridNextBob : gridNextBob[key]=[]
             gridNextBob[key].append(Bob())
             l=len(gridNextBob[key])
             gridNextBob[key][l-1].energy = birthEnergy
             print("birth")
+
+    def energyControl(self, key): #if the bob can eat more than his max energy, he leaves some food on the ground and bottoms out at the max lever of energy.
+        if self.energy>maxEnergy: #on regarde si le bob a atteint son niveau d'énergie max. Si il a trop mangé, il recrache le supplément et reste au niveau maximal. Le supplément revient dans la case ou se situe le bob.
+            if key not in gridFood : gridFood[key]=0
+            gridFood[key]+=self.energy-maxEnergy
+            self.energy=maxEnergy
+            print("A bob has reach his max level of energy")
+            self.partheno(key)
+        elif key in gridFood : #si l'énergie ne dépasse pas le max et qu'il existe de l'énergie (égale à 0 si tout fonctionne) dans la case de coordonnée key, on supprime la case key du dictionnaire gridFood.
+            del gridFood[key]
 
 
     def hunt(self, key): #OK !
@@ -55,8 +66,9 @@ class Bob:
                 if gridBob[key][i].size<2/3*self.size:
                     #EAT THE BOB
                     self.energy += gridBob[key][i].energy
+                    self.energyControl(key)
                     del gridBob[key][i] #Je peux supprimer ce bob car la taille du dictionnaire ne sera pas modifiée.
-                    self.partheno(key)
+                    print("1 bob murdered")
                     return True
         return False
 
@@ -65,13 +77,24 @@ class Bob:
         return False
 
 
-    def eat(self, key): #MARTIN
-        return False
+    def eat(self, key): #TITOUAN
+        if key not in gridFood or self.energy==maxEnergy : return False
+        self.energy += gridFood[key]
+        self.energyControl(key)
+        print("Eat food grrrr")
+        return True
+
 
 
     def randomMove(self, key): #TITOUAN
+        self.energy-=1 
+        if self.energy<=0: #Verifier si le bob a toujours de l'energie. Sinon il meurt.
+            gridBob[key].remove(self)
+            print("A Bob starved to death...")
+            return
+
         moved=False
-        while moved==False:
+        while moved==False: #tant qu'on a pas effectué un déplacement, on cherche un déplacement valide.
             (x,y)=key
             if randint(0,1) : #choix random vertical ou horizontal.
                 x+=randint(1,2)*2-3 #aléatoirement +1 ou -1 à la coordonnée x
@@ -82,10 +105,11 @@ class Bob:
                 if y<=gridSize-1 and y>=0:
                     moved = True
 
+        #on applique le déplacement valide trouvé auparavant.
         if (x,y) not in gridNextBob : gridNextBob[(x,y)]=[]
         gridNextBob[(x,y)].append(self)
         gridBob[key].remove(self)
-        return True
+        return
 
 
 
@@ -163,7 +187,7 @@ def initialisation():
     printGridFood()
 
 
-def removeDeadBoddies():
+def removeDeadBoddies(): #remove the empty bob arrays in the dictionnary gridBob.
     keysToRemove = []
     for key, bobs in gridBob.items():
         if len(bobs)==0 : keysToRemove.append(key)
@@ -172,10 +196,10 @@ def removeDeadBoddies():
 
 
 
-def mergeGrids():
+def mergeGrids(): #fusionne le dictionnaire gridNextBob (qui représente les bobs qui viennent de se déplacer) dans le dictionnaire gridBobs
     for key in gridNextBob:
         if key not in gridBob : gridBob[key]=[]
-        gridBob[key]=gridNextBob[key]
+        for bob in gridNextBob[key] : gridBob[key].append(bob)
 
 
 def tick():
@@ -195,11 +219,11 @@ def tick():
             if not bob.eat(key): #Eat food
                 if not bob.hunt(key): #Hunt other bobs
                     if not bob.fuck(key): #Fuck
-                        if not bob.randomMove(key): #Move randomly
-                            print("ERREUR : UN BOB N A PAS EFFECTUE SON ACTION !")
+                        bob.randomMove(key) #Move randomly
     removeDeadBoddies()
     mergeGrids()
     gridNextBob.clear()
+    sleep(0.1)
     return bobcount
     
 
@@ -207,13 +231,15 @@ def tick():
 
 initialisation()
 foodSpawn()
-for i in range (1) :
+for i in range (5) :
+    print("\n\t******** DAY ********\n")
     foodSpawn()
     for t in range (ticksPerDay):
-        print("TICK")
+        print("\t\ttick")
         bobcount = tick()
-        printGridBob()
+        #printGridBob()
 
 #printGridBob()
 #printGridFood()
 print("Il reste", bobcount, "Bobs en vie.")
+#printGridFood()
