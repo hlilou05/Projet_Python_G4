d7bfc8533cd98bdeef9eb559df8405929e1a9357
YasserGuerram <144919668+YasserGuerram@users.noreply.github.com>
2024-01-13 16:19:31 +0100

    Update python.py

--- python.py
+++ python.py
@@ -1,201 +1,19 @@
-# import pygame
-# import sys
-
-# pygame.init()
-
-# largeur, hauteur = 800, 600
-# fenetre = pygame.display.set_mode((largeur, hauteur))
-# pygame.display.set_caption("PROJET_PYTHON_2024")
-
-# bleu = (0, 0, 250)
-# noir = (0, 0, 0)
-# rouge = (250, 0, 0)
-# vert = (0, 250, 0)
-
-# music_song = pygame.mixer.Sound("music_pygame.mp3")
-# music_song.play()
-
-# police = pygame.font.Font(None, 36)
-
-# def afficher_texte(texte, x, y):
-#     texte_surface = police.render(texte, True, noir)
-#     texte_rect = texte_surface.get_rect()
-#     texte_rect.center = (x, y)
-#     fenetre.blit(texte_surface, texte_rect)
-
-# def main_menu():
-#     while True:
-#         for event in pygame.event.get():
-#             if event.type == pygame.QUIT:
-#                 pygame.quit()
-#                 sys.exit()
-#             if event.type == pygame.MOUSEBUTTONDOWN:
-#                 x, y = pygame.mouse.get_pos()
-#                 if 300 <= x <= 500:
-#                     if 200 <= y <= 250:
-#                         print("Start")
-#                         interface_jeu()
-#                     elif 300 <= y <= 350:
-#                         print("Options")
-#                     elif 400 <= y <= 450:
-#                         pygame.quit()
-#                         sys.exit()
-
-#         background = pygame.image.load("image_path.jpg")
-#         background = pygame.transform.scale(background, (fenetre.get_width(), fenetre.get_height()))
-#         fenetre.blit(background, (0, 0))
-
-#         afficher_texte("GAME", largeur // 2, 100)
-#         pygame.draw.rect(fenetre, rouge, (300, 200, 200, 50))
-#         afficher_texte("START", largeur // 2, 225)
-#         pygame.draw.rect(fenetre, rouge, (300, 300, 200, 50))
-#         afficher_texte("OPTIONS", largeur // 2, 325)
-#         pygame.draw.rect(fenetre, rouge, (300, 400, 200, 50))
-#         afficher_texte("QUIT", largeur // 2, 425)
-
-#         pygame.display.update()
-# TILE_SIZE = 16
-# class Tile():
-#     def __init__(self, x, y):
-#         self.image = pygame.image.load("Tile32x32.png")
-#         self.rect = self.image.get_rect()
-#         self.rect.x = x
-#         self.rect.y = y
-
-
-# def iso_coord(x, y):
-#     return (x - y, (x + y) / 2)
-
-# def update_window(fenetre):
-#     background = pygame.image.load("arrière_plan.jfif")
-#     background = pygame.transform.scale(background, (fenetre.get_width(), fenetre.get_height()))
-#     fenetre.blit(background, (0, 0))
-    
-
-# def display_tiles(screen, value):
-#     screen_x, screen_y = screen.get_size()
-#     for y in range(-value // 2, value // 2):
-#         for x in range(-value // 2, value // 2):
-#             new_tile = Tile(screen_x // 4 + screen_y // 2 + x * TILE_SIZE,
-#                             screen_y // 2 - screen_x // 4 + y * TILE_SIZE)
-#             screen.blit(new_tile.image, iso_coord(new_tile.rect.x, new_tile.rect.y))
-
-# def interface_jeu():
-#     jeu_en_cours = True
-#     while jeu_en_cours:
-#         for event in pygame.event.get():
-#             if event.type == pygame.QUIT:
-#                 pygame.quit()
-#                 sys.exit()
-        
-#         done = False
-        
-        
-#         update_window(fenetre)
-#         display_tiles(fenetre, 20)
-#         pygame.display.update()
-        
-
-
-# # Point d'entrée du programme
-# if __name__ == "__main__":
-#     main_menu()
-
-# import pygame
-# import sys
-
-# pygame.init()
-
-# # Couleurs
-# BLANC = (255, 255, 255)
-# NOIR = (0, 0, 0)
-# GRIS = (200, 200, 200)
-
-# # Initialisation de Pygame
-# largeur, hauteur = 600, 300
-# fenetre = pygame.display.set_mode((largeur, hauteur))
-# pygame.display.set_caption("Affichage avec champ de saisie")
-
-# # Police
-# police = pygame.font.Font(None, 36)
-
-# # Champ de texte
-# champ_texte = pygame.Rect(300, 100, 300, 50)
-# couleur_active = pygame.Color('dodgerblue2')
-# couleur_inactive = pygame.Color('lightskyblue3')
-# couleur = couleur_inactive
-# texte = ''
-# actif = False
-
-# def affiche(var):
-#     fenetre.fill(BLANC)
-
-#     # Affichage de la chaîne de caractères
-#     var_surface = police.render(var, True, NOIR)
-#     fenetre.blit(var_surface, (50, 100))
-
-#     # Champ de texte
-#     pygame.draw.rect(fenetre, couleur, champ_texte, 2)
-#     texte_surface = police.render(texte, True, NOIR)
-#     largeur_texte = max(200, texte_surface.get_width()+10)
-#     champ_texte.w = largeur_texte
-#     fenetre.blit(texte_surface, (champ_texte.x+5, champ_texte.y+5))
-
-#     pygame.display.flip()
-
-# # Boucle principale
-# while True:
-#     for event in pygame.event.get():
-#         if event.type == pygame.QUIT:
-#             pygame.quit()
-#             sys.exit()
-#         if event.type == pygame.MOUSEBUTTONDOWN:
-#             if champ_texte.collidepoint(event.pos):
-#                 actif = not actif
-#             else:
-#                 actif = False
-#             couleur = couleur_active if actif else couleur_inactive
-#         if event.type == pygame.KEYDOWN:
-#             if actif:
-#                 if event.key == pygame.K_RETURN:
-#                     print(texte)
-#                     texte = ''
-#                 elif event.key == pygame.K_BACKSPACE:
-#                     texte = texte[:-1]
-#                 else:
-#                     texte += event.unicode
-
-#     affiche("Variable")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 import pygame
 import sys
+from random import randint
 
 pygame.init()
 
 largeur, hauteur = 800, 600
 fenetre = pygame.display.set_mode((largeur, hauteur))
 pygame.display.set_caption("PROJET_PYTHON_2024")
+done=False
 
 bleu = (0, 0, 250)
 noir = (0, 0, 0)
 rouge = (250, 0, 0)
 vert = (0, 250, 0)
+gridSize = 10 
 
 # Utilisez des noms de variables différents, sinon vous écrasez la variable noir
 # Déplacez la musique dans la fonction principale pour éviter des problèmes de lecture avant l'initialisation de Pygame
@@ -213,6 +31,7 @@ def main_menu():
     music_song.play()
 
     while True:
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -253,6 +72,8 @@ class Tile():
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
+        self.bob = None
+        self.food = None
 
 def iso_coord(x, y):
     return (x - y, (x + y) / 2)
@@ -270,18 +91,139 @@ def display_tiles(screen, value):
                             screen_y // 2 - screen_x // 4 + y * TILE_SIZE)
             screen.blit(new_tile.image, iso_coord(new_tile.rect.x, new_tile.rect.y))
 
+
+class Bob(pygame.sprite.Sprite):
+   def __init__(self):
+       super().__init__()
+       self.velocity = 15
+       self.size = randint(10, 20)
+       self.energy = 100
+       self.maxenergy = 200
+       self.perception = 0
+       self.mass= 1
+       self.image = pygame.image.load("bob.png")
+       self.image_blue = pygame.image.load("bob.blue.png")
+       self.image_rouge = pygame.image.load("bob.rouge.png")
+       self.image = pygame.transform.scale(self.image, (16, 16))
+       self.image_blue = pygame.transform.scale(self.image_blue, (16, 16))
+       self.image_rouge = pygame.transform.scale(self.image_rouge, (16, 16))
+       self.rect = self.image.get_rect()
+       self.rect.x = 300
+       self.rect.y = 400
+   def move_right(self): 
+       if self.rect.x + self.velocity < fenetre.get_width() - self.rect.width:
+            self.rect.x += 1
+            fenetre.fill((255, 255, 255))
+            self.rect.x+=self.velocity
+            display_tiles(fenetre, 20)
+   def move_left(self):
+       self.rect.x-= self.velocity
+       display_tiles(fenetre, 20)
+   def move_up(self):
+       fenetre.fill((255, 255, 255))
+       self.rect.y -= self.velocity
+       display_tiles(fenetre, 20)
+   def move_down(self):
+       fenetre.fill((255, 255, 255))
+       self.rect.y += self.velocity
+       display_tiles(fenetre, 20)
+   
+   def update_color(self):
+       if self.velocity > 20:
+           self.image = self.image_blue
+       if self.mass >10:
+           self.image = self.image_rouge
+
+
+class Game:
+    def __init__(self):
+        self.grid = [[Tile(x, y) for x in range(gridSize)] for y in range(gridSize)]
+        self.bob = Bob()
+        self.all_food = pygame.sprite.Group()
+        self.foodSpawn(10)
+
+    def foodSpawn(self, quantity):
+        for _ in range(quantity):
+            a = randint(0, gridSize - 1)
+            b = randint(0, gridSize - 1)
+            if not self.grid[a][b].food:
+                self.grid[a][b].food = Food(a, b)
+                self.all_food.add(self.grid[a][b].food)
+                # ... position the food on the grid ...
+
+    def update_bob_position(self, x, y):
+        if self.bob.rect.x != x or self.bob.rect.y != y:
+            self.grid[x][y].bob = self.bob
+            if self.bob.rect.x is not None and self.bob.rect.y is not None:
+                self.grid[self.bob.rect.x][self.bob.rect.y].bob = None
+            self.bob.rect.x, self.bob.rect.y = x, y
+
+
+
+
+
+class Food(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super().__init__()
+        self.energy = 100
+        self.image = pygame.image.load("nourriture.png")
+        self.image = pygame.transform.scale(self.image, (24, 24))
+        self.rect = self.image.get_rect()
+        self.rect.x, self.rect.y = 310, 400
+
+# # Charger notre bob
+# update_window(fenetre)
+# display_tiles(fenetre, 20)
+# pygame.display.update()
+# # Charger le jeu
+game = Game()
+
+
+# for food in game.all_food:
+#         fenetre.blit(food.image, food.rect)
+
+
+
 def interface_jeu():
     jeu_en_cours = True
-    while jeu_en_cours:
+    done = False
+
+    while jeu_en_cours and not done:
+        # for event in pygame.event.get():
+        #     if event.type == pygame.QUIT:
+        #         pygame.quit()
+        #         sys.exit()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    game.bob.move_right()
+                elif event.key == pygame.K_LEFT:
+                    game.bob.move_left()
+                elif event.key == pygame.K_UP:
+                    game.bob.move_up()
+                elif event.key == pygame.K_DOWN:
+                    game.bob.move_down()
 
-        done = False
+       
+        # Effacer l'écran
         update_window(fenetre)
+
+        # Afficher les tuiles du sol
         display_tiles(fenetre, 20)
+
+        # Afficher l'image de Bob
+        fenetre.blit(game.bob.image, game.bob.rect)
+        for food in game.all_food:
+            fenetre.blit(food.image, food.rect)
+
         pygame.display.update()
+
+
+
 
 # Champs de texte
 champs = [pygame.Rect(200, 50 + i * 70, 300, 50) for i in range(8)]
@@ -347,85 +289,23 @@ def affiche_formulaire(variables):
 if __name__ == "__main__":
     main_menu()
 
-        
-
-
-# # Point d'entrée du programme
-# if __name__ == "__main__":
-#     main_menu()
 
 
 
-# import pygame
-# import sys
 
-# pygame.init()
 
-# # Couleurs
-# BLANC = (255, 255, 255)
-# NOIR = (0, 0, 0)
-# GRIS = (200, 200, 200)
 
-# # Initialisation de Pygame
-# largeur, hauteur = 800, 600
-# fenetre = pygame.display.set_mode((largeur, hauteur))
-# pygame.display.set_caption("Formulaire avec champs de saisie")
 
-# # Police
-# police = pygame.font.Font(None, 36)
 
-# # Champs de texte
-# champs = [pygame.Rect(200, 50 + i * 70, 300, 50) for i in range(8)]
 
-# couleur_active = pygame.Color('dodgerblue2')
-# couleur_inactive = pygame.Color('lightskyblue3')
-# couleur = couleur_inactive
-# textes = [''] * 8
-# actif_champ = None
 
-# def affiche_formulaire(variables):
-#     fenetre.fill(BLANC)
 
-#     for i, (variable, champ) in enumerate(zip(variables, champs)):
-#         # Affichage de la variable
-#         var_surface = police.render(variable, True, NOIR)
-#         fenetre.blit(var_surface, (50, 50 + i * 70))
 
-#         # Champ de saisie
-#         pygame.draw.rect(fenetre, couleur, champ, 2)
-#         texte_surface = police.render(textes[i], True, NOIR)
-#         largeur_texte = max(200, texte_surface.get_width() + 10)
-#         champ.w = largeur_texte
-#         fenetre.blit(texte_surface, (champ.x + 5, champ.y + 5))
 
-#     pygame.display.flip()
 
-# # Liste de variables
-# variables = ['G.S.Larg', 'G.S.long', 'Energy', 'Velocity', 'Min energy', 'The mass', 'score Bp', 'memory']
 
-# # Boucle principale
-# while True:
-#     for event in pygame.event.get():
-#         if event.type == pygame.QUIT:
-#             pygame.quit()
-#             sys.exit()
-#         if event.type == pygame.MOUSEBUTTONDOWN:
-#             for i, champ in enumerate(champs):
-#                 if champ.collidepoint(event.pos):
-#                     actif_champ = i
-#                     couleur = couleur_active
-#                 else:
-#                     couleur = couleur_inactive
-#         if event.type == pygame.KEYDOWN:
-#             if actif_champ is not None:
-#                 if event.key == pygame.K_RETURN:
-#                     print("Variable {}: {}".format(actif_champ + 1, textes[actif_champ]))
-#                     textes[actif_champ] = ''
-#                 elif event.key == pygame.K_BACKSPACE:
-#                     textes[actif_champ] = textes[actif_champ][:-1]
-#                 else:
-#                     textes[actif_champ] += event.unicode
 
-#     affiche_formulaire(variables)
+
+
 
 
