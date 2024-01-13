85d199845834d1b5d3ac98e62aa1c7227541e39b
UserMika <71926176+UserMika@users.noreply.github.com>
2024-01-13 17:57:56 +0100

    ajout de documentation

--- memory + move_perception.py
+++ memory + move_perception.py
@@ -1,50 +1,71 @@
+#saves the path taken by the bob in self.path 
+# bob_co : bob's coordinates
+#self.path : list of lists containing at index 0 the coordinates of a tile and at index 1 the amount of food in it.
+# self.remembering_food : a boolean, true if the bob is currently remembering a food not in his perception, false if not
+#self.possiblefood : variable equals to the food currently remembered
 
-
-def use_memory(self):
-    bob_co = (0,0)
-    for coord in GameWorld.gridBob.keys():
-        if self == GameWorld.gridBob[coord]:
-            bob_co = coord
-            break
+def use_memory(self,bob_co):
+    # if there is some food on the bob's coordinates, save its amount
     if GameWorld.gridFood(bob_co):
         self.path.append([bob_co,GameWorld.gridFood(bob_co)])
     else:
+        # no food on bob's coordinates, amount of food = 0
         self.path.append([bob_co, 0])
-    if len(self.path) > 2 * (self.memory-self.remembering_food):
-        del self.path[0] 
+    #if the coordinates of the remembered food contain a different amount of food (food eaten or food that respawned)
+    if self.perception["food"].has_key(self.possiblefood[0]) & self.perception["food"][1] != self.possiblefood[1]:
+        self.possiblefood = (self.possiblefood[0], 0)
+    # find the biggest food in the bob's perception
     for food in self.perception["food"].items():
         if food[1] > self.possiblefood[1]:
             self.possiblefood = food
+    #determines whether the bob needs to remember the food.
     if self.possiblefood not in self.perception["food"].items():
         self.remembering_food = 1
     else:
         self.remembering_food = 0 
+    # if the bob already remembers at its maximum capacity, remove the last remembered tiles
+    while len(self.path) > 2 * (self.memory-self.remembering_food):
+        del self.path[0] 
 
+#function to find the best food to hunt in the bob's perception
+#self.currenttarget : variable which will contain the best food to hunt
 def deplace_perception(self, coord):
+    (x,y) = (0,0)
+    #initializing the variable that will be compared with the Manhattan distance between bob's coordinates and the static foods' coordinates
     diff = self.Bp*2
     maxfood = 0
+    #determines nearest static food with the biggest energy
     for food in self.perception["food"].items():
-        if diff > abs(food[0][0]-coord[0])+abs(food[0][1]-coord[1]):
+        if diff >= abs(food[0][0]-coord[0])+abs(food[0][1]-coord[1]):
             if maxfood <= food[1]:
                 diff = abs(food[0][0]-coord[0])+abs(food[0][1]-coord[1])
                 self.currenttarget = food[0]
                 maxfood = food[1]
     if self.currenttarget != None:
-        reach_target(self,coord)
+       (x,y) = reach_target(self,coord)
+        if (x,y) not in GameWorld.gridBob : 
+            GameWorld.gridBob[(x,y)]=[]
+        GameWorld.gridBob[(x,y)].append(self)
+        GameWorld.gridBob[Key].remove(self)
         self.currenttarget = None
     else:
         maxfood = self.energy
+        #determines the nearest bob with the lowest energy
         for bob in self.perception["bob-"].items():
-            if diff > abs(bob[0][0]-coord[0])+abs(bob[0][1]-coord[1]):
+            if diff >= abs(bob[0][0]-coord[0])+abs(bob[0][1]-coord[1]):
                 if maxfood >= bob[1]:
                     diff = abs(bob[0][0]-coord[0])+abs(bob[0][1]-coord[1])
                     self.currenttarget = bob[0]
                     maxfood = bob[1]
         if self.currenttarget != None:
-            reach_target(self,coord)
+            (x,y) = reach_target(self,coord)
+            if (x,y) not in GameWorld.gridBob : 
+                GameWorld.gridBob[(x,y)]=[]
+            GameWorld.gridBob[(x,y)].append(self)
+            GameWorld.gridBob[Key].remove(self)
             self.currenttarget = None
             
-
+    #function to randomly decrease the distance between the bob and its hunted food.
     def reach_target(self,coord):
         (x,y) = coord
         if self.currenttarget[0] == coord[0]:
@@ -72,3 +93,4 @@ def deplace_perception(self, coord):
                 x -= 1
             else:
                 y += 1
+        return (x,y)
