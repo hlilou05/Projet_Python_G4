af16e30d4a475409bccc7c93ce7f81a7390b4097
MartinFid <83142999+MartinFid@users.noreply.github.com>
2024-01-28 21:05:37 +0100

    Add files via upload

---
+++ test_yasser/main.py
@@ -0,0 +1,12 @@
+from app.World import *
+
+def main():
+    pygame.init()
+    game = World()
+    game.window.display_menu()
+    # while game.isRunning:
+    #     game.run_game()
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
